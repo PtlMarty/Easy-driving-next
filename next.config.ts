a41d81cb2a2bf27ee17ce431f0b95839f2b6951b
portal martin <ptl_martin@yahoo.co.jp>
2024-10-28 21:49:35 +0900

    try fix 1

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
-import type { NextConfig } from "next";
-
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-  // Configure le prefixe des assets pour le déploiement sur GitHub Pages
-  assetPrefix: isProd ? "/Easy-driving-next/" : "",
+const nextConfig = {
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
   images: {
-    // Désactive l'optimisation des images pour éviter les problèmes sur GitHub Pages
     unoptimized: true,
   },
-  // Autres options de configuration ici
 };
 
 export default nextConfig;
